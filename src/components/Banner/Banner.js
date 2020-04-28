391221b6aa10795f89b3a0d62fe1353b858210ae
rakshitha17 <rakshitha@moonraft.com>
2020-04-28 21:14:28 +0530

    making banner functional component

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,20 +1,35 @@
-import './Banner.css'
-import React from 'react';
-const banner = () => {
-    return(
-        <>
-        <div className='banner'>
-            <div className='bannerText'>
-                <h1>A free API for data on the Coronavirus</h1>
-                <p>Access data on COVID19 through an easy API for free. Build dashboards, mobile apps or integrate in to other applications. Data is sourced from &nbsp; 
-                    <a href="/" target="_blank">Johns Hopkins CSSE</a>
-                </p>
-            </div>
-            <div className="bannerImage"></div>
-        </div>
-        <div className="globalCount">1,347,343 <p className="globalSpan">people are affected by covid-19</p></div>
-       </>
+import React, { useState, useEffect } from 'react';
+import './Banner.css';
+const useFetch = (url) => {
+  const [data, updateData] = useState([]);
+  // empty array as second argument equivalent to componentDidMount
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url);
+      const json = await response.json();
+      updateData(json.Global);
+    }
+    fetchData();
+  }, [url]);
+  return {data};
+};
 
+const Banner = () => {
+    const URL = 'https://api.covid19api.com/summary';
+    const result = useFetch(URL);
+    return (
+            <>
+            <div className='banner'>
+                <div className='bannerText'>
+                    <h1>A free API for data on the Coronavirus</h1>
+                    <p>Access data on COVID19 through an easy API for free. Build dashboards, mobile apps or integrate in to other applications. Data is sourced from &nbsp; 
+                        <a href="/" target="_blank">Johns Hopkins CSSE</a>
+                    </p>
+                </div>
+                <div className="bannerImage"></div>
+            </div>
+            <div className="globalCount">{result["data"].TotalConfirmed} <p className="globalSpan">people are affected by covid-19</p></div>
+            </>
     );
 }
-export default banner;
+export default Banner;
